ba0629c66f462dfef283e4632845a6f57df1f1be
Matthew Heick <github@unliterate.net>
2020-11-22 22:23:40 +0000

    initial framing and fun

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tell-Tale-Text</title>
+		<script src="./story-time.js"></script>
+		<link rel="stylesheet" href="./pretty.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="theme-color" content="#000000" />
+	</head>
+	<body>
+		<div class="div-header">
+			<div class="div-logo"><img src="ttt.png" /></div>
+			<div class="div-logo-text">Tell Tale Text</div>
+		</div>
+		<div class="div-body" id="topmost">
+Once upon a time, in a time a long long time ago, once, and only once there was this amazing thing that was a thing that was amazing...
+		</div>
+		<div class="div-more" id="continue">
+		</div>
+	</body>
+</html>
